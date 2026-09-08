41d328055ba61ce99105c1e72df1bde6f5d39bd9
Angel David Gomez <ag1377934@gmail.com>
2026-09-08 08:40:04 -0500

    correcion de codigo y correion de usuario y correo de VS

--- pokedex-react/src/services/PokeApi.js
+++ pokedex-react/src/services/PokeApi.js
@@ -4,10 +4,8 @@ export const buscarPokemon = async (nombre) => {
     const respuesta = await fetch(
         `${API_URL}/pokemon/${nombre.toLowerCase()}`
     );
-
     if (!respuesta.ok) {
         throw new Error("Pokémon no encontrado");
     }
-
     return await respuesta.json();
 };
